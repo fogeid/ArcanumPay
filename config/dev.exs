0de160fbfee056df9ca6c9828b5de37aa03a877e
Diego Batista <diego.silva@aluno.ic.ufmt.br>
2021-02-23 15:52:26 -0400

    update

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :rocketpay, Rocketpay.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "admin",
   database: "rocketpay_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
